34ef6d582b657f40903489989c4c55eedf78cc46
Vladyslav Serhiienko <ext-vladyslav.serhiienko@here.com>
2017-02-16 00:47:21 +0200

    Improved CompileShaders.js further.

--- FbxPipeline/scripts/CompileShaders.js
+++ FbxPipeline/scripts/CompileShaders.js
@@ -1,7 +1,15 @@
+
+//
+// A script for shader compilation using shaderc tool by bgfx.
+// Generates all possible shaders
+// For include it uses ../shaders/shared and bgfx/src folders.
+// All the output goes to assets/shaders/include and runtime folders.
+// Output folders will be created automatically if needed.
+//
+
 const fs = require('fs');
 const path = require('path');
 const exec = require('child_process').exec;
-const spawn = require('child_process').spawnSync;
 
 function removeExtension(filename) {
     var lastDotPosition = filename.lastIndexOf(".");
@@ -16,23 +24,33 @@ function removeFile(filename) {
     else return filename.substr(0, lastDotPosition);
 }
 
+function createFolderIfNeeded(foldername) {
+    if (false === fs.existsSync(foldername)){
+        fs.mkdirSync(foldername);
+    }
+}
+
 var shaderc = removeFile(process.argv[1]);
 var shaderDir = shaderc + '/shaders';
 if (process.argv.length >= 3)
     shaderDir = process.argv[2];
 
-var rutimeDir = shaderDir + '/runtime/';
-var rutimeIncludeDir = shaderDir + '/include/';
+var rutimeDir = shaderc + '/../assets/shaders/runtime/';
+var rutimeIncludeDir = shaderc + '/../assets/shaders/include/';
 
-if (!fs.existsSync(rutimeDir)){
-    fs.mkdirSync(rutimeDir);
-}
+createFolderIfNeeded(shaderc + '/../assets/shaders/');
+createFolderIfNeeded(shaderc + '/../assets/shaders/runtime/');
+createFolderIfNeeded(shaderc + '/../assets/shaders/include/');
 
-if (!fs.existsSync(rutimeIncludeDir)){
-    fs.mkdirSync(rutimeIncludeDir);
-}
+var platformProfiles = ['windows -p {{hlsl}}s_3_0 -O 3',
+                        'windows -p {{hlsl}}s_5_0 -O 3',
+                        'nacl',
+                        'android',
+                        'linux -p 120',
+                        'linux -p spirv',
+                        'osx -p metal',
+                        'asm.js'];
 
-var platformProfiles = ['windows', 'android', 'osx', 'ios'];
 var commandTemplate = 'shaderc -f {{shaderFilePath}} -o {{binFilePath}} {{bin2c}} ' +
                       '-i ../shaders/shared -i ../../ThirdParty/bgfx/src --verbose ' +
                       '--type {{shaderType}} --platform {{platformProfile}}';
@@ -41,11 +59,18 @@ fs.readdir(shaderDir, (err, files) => {
     files.forEach(fileName => {
          if (path.extname(fileName) == ".sc") {
             var shaderType = null;
+            var hlslShaderType = null;
             var shaderFilePref = fileName.substring(0, 3);
+
             if (shaderFilePref == "fs_") {
                 shaderType = "fragment";
+                hlslShaderType = "p";
             } else if (shaderFilePref == "vs_") {
                 shaderType = "vertex";
+                hlslShaderType = "v";
+            } else if (shaderFilePref == "cs_") {
+                shaderType = "compute";
+                hlslShaderType = "c";
             }
 
             if (shaderType != null) {
@@ -63,11 +88,17 @@ fs.readdir(shaderDir, (err, files) => {
                 console.log('Inc (T): ', incCmdTemplate);
 
                 platformProfiles.forEach((value, index, array) => {
-                    var binFilePath = rutimeDir + removeExtension(fileName) + '_' + value + '.bin';
-                    var incFilePath = rutimeIncludeDir + removeExtension(fileName) + '_' + value + ".bin.h";
+                    var extensionValue = value.replace('.', '_');
+                        extensionValue = extensionValue.replace(/ /g, '_');
+                        extensionValue = extensionValue.replace(/-/g, '_');
+                        extensionValue = extensionValue.replace('{{hlsl}}', hlslShaderType);
+                    var platformProfile = value.replace('{{hlsl}}', hlslShaderType);
 
-                    var binCmd = binCmdTemplate.replace('{{binFilePath}}', binFilePath).replace('{{platformProfile}}', value);
-                    var incCmd = incCmdTemplate.replace('{{binFilePath}}', incFilePath).replace('{{platformProfile}}', value);
+                    var binFilePath = rutimeDir + removeExtension(fileName) + '_' + extensionValue + '.bin';
+                    var incFilePath = rutimeIncludeDir + removeExtension(fileName) + '_' + extensionValue + ".bin.h";
+
+                    var binCmd = binCmdTemplate.replace('{{binFilePath}}', binFilePath).replace('{{platformProfile}}', platformProfile);
+                    var incCmd = incCmdTemplate.replace('{{binFilePath}}', incFilePath).replace('{{platformProfile}}', platformProfile);
 
                     console.log('Bin: ', binCmd);
                     console.log('Inc: ', incCmd);
